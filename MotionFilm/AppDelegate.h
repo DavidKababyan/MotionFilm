6f228883b544db3ba4af811c5d55d4c7f571a2ba
DavidKababyan <David@Davids-MacBook-Pro.local>
2014-10-31 07:52:33 +0200

    Initial Commit

---
+++ MotionFilm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MotionFilm
+//
+//  Created by David Kababyan on 10/31/14.
+//  Copyright (c) 2014 David Kababyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
